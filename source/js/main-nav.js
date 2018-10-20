0e2788094daa334ad34b15249bc35b865513fa28
Ewokinator <whatacrap@gmail.com>
2018-10-20 13:51:26 +0300

    Сетки + детальное оформление

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,11 +2,15 @@ var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = document.querySelector('.page-header__toggle');
 
 mainNavToggle.addEventListener('click', function() {
-  if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
-  } else {
+  if (mainNav.classList.contains('main-nav--no-js') && mainNavToggle.classList.contains('page-header__toggle--no-js')) {
+    mainNav.classList.remove('main-nav--no-js');
     mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
+    mainNavToggle.classList.remove('page-header__toggle--no-js');
+    mainNavToggle.classList.add('page-header__toggle--closed');
+  } else {
+    mainNav.classList.add('main-nav--no-js');
+    mainNav.classList.remove('main-nav--closed');
+    mainNavToggle.classList.add('page-header__toggle--no-js');
+    mainNavToggle.classList.remove('page-header__toggle--closed');
   }
 });
